73796f09cab3c8fd078404dbfdda8ae1d75ab220
YaleX <alexecorzo@hotmail.com>
2017-07-18 22:43:52 +0000

    Review Model and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   devise_for :users
-  resources :books
+  
+  resources :books do
+    resources :reviews
+  end
+  
   root "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
